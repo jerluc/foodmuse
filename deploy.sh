615c4c6e0ee2b945be6bbd2c494244f71acfe61e
Jeremy Lucas <jeremyalucas@gmail.com>
2013-06-22 20:35:02 -0700

    Updating scripts for deployments

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+scp target/foodmuse-distro-*.jar root@foodmuse.co:/usr/local/www/foodmuse/lib/
+scp prod.yml root@foodmuse.co:/usr/local/www/foodmuse/conf/
+scp fm-start.sh root@foodmuse.co:/usr/local/www/foodmuse/bin/
--- fm-start.sh
+++ fm-start.sh
@@ -1,1 +1,2 @@
-java -jar target/foodmuse-distro-*.jar server prod.yml
+CURRENT_DIR=`basename $0`
+java -jar $CURRENT_DIR/lib/foodmuse-distro-*.jar server $CURRENT_DIR/conf/prod.yml
