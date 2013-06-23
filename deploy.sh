2fc00f8718cddb00570f9d3668f44a656f41c903
Jeremy Lucas <jeremyalucas@gmail.com>
2013-06-22 20:40:40 -0700

    Removing ignored resource

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-scp target/foodmuse-distro-*.jar root@foodmuse.co:/usr/local/www/foodmuse/lib/
-scp prod.yml root@foodmuse.co:/usr/local/www/foodmuse/conf/
-scp fm-start.sh root@foodmuse.co:/usr/local/www/foodmuse/bin/
